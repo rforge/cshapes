c785202bfd2db4c7508b7c97f29a170810e04c90
nilsw <nilsw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-25 18:12:23 +0000

    Website update.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cshapes@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> The main cshapes users website is located at <a href="http://nils.weidmann.ws/projects/cshapes">http://nils.weidmann.ws/projects/cshapes</a> </p>
-
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> For more information, please see the main cshapes users website at <a href="http://nils.weidmann.ws/projects/cshapes">http://nils.weidmann.ws/projects/cshapes</a>. </p>
 
 </body>
 </html>
