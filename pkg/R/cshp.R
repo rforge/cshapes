938d3a9060639d39c8e02775c74271d6e3fac9cb
nilsw <nilsw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 08:30:58 +0000

    First complete version of 0.4-1. Needs to be tuned due to performance issues.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cshapes@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cshp.R
+++ pkg/R/cshp.R
@@ -5,7 +5,7 @@ cshp <- function(date=NA, useGW=TRUE) {
     	stop("date is not of type Date")
   	}
   
-  	if (!is.na(date) && (date < as.Date("1946-1-1") | date > as.Date("2008-6-30"))) {
+  	if (!is.na(date) && (date < as.Date("1946-1-1") | date > as.Date("2012-6-30"))) {
     	stop("Specified date is out of range")
   	}
 	
